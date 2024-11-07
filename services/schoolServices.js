8bd39d6b5bac75659825a96aa4cbbe9c3f692a11
yashwanth <yashwanth.Talagavadi@rhibhus.com>
2024-11-07 15:17:39 +0530

    bug fixes

--- services/schoolServices.js
+++ services/schoolServices.js
@@ -4,3 +4,6 @@ const {schoolRepository,classRepository} = require("../repository")
 const constant = require('../constants/constant');
 const helper = require('../helper/helper');
 
+exports.fetchSchoolDetails = async (request) => {
+    return await schoolRepository.getSchoolDetailsById2(request);
+}
