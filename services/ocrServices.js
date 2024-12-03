3b97572df3e53ea890e219c0711c316297827103
yashwanth <yashwanth.Talagavadi@rhibhus.com>
2024-12-03 11:24:09 +0530

    u

--- services/ocrServices.js
+++ services/ocrServices.js
@@ -122,27 +122,43 @@ async function convertImageToBase64(imageUrl) {
 
   async function extractTextAndEquations(imageUrl ,predictiveText ) {
     try {
-      const promptText = predictiveText && predictiveText === 'Yes'
-              ? 'Extract text, images, and equations from the image. Also, read the page number. If there are spelling or grammar mistakes, correct them and highlight the corrected words in red using inline CSS (e.g., <span style="color:red;">corrected word</span>).'
-              : 'Extract text, images, and equations from the image. Also, read the page number.';
-      
+
       const base64Image = await convertImageToBase64(imageUrl);
   
       if (base64Image) {
-        const response = await openai.chat.completions.create({
-          model: 'gpt-4o',  
-          messages: [
-            {
-              role: 'user',
-              content: [
-                { type: 'text', text: promptText },
-                { type: 'image_url', image_url: { url: base64Image } },
-              ],
-            },
-          ],
-        });
+        let response ;
+        if( predictiveText && predictiveText === 'Yes')
+        {
+          response = await openai.chat.completions.create({
+            model: 'gpt-4o',  
+            messages: [
+              {
+                role: 'user',
+                content: [
+                  { type: 'text', text: 'Extract text, images, and equations from the image. Also, read the page number. If there are spelling or grammar mistakes, correct them and highlight the corrected words in red using inline CSS (e.g., <span style="color:red;">corrected word</span>).' },
+                  { type: 'image_url', image_url: { url: base64Image } },
+                ],
+              },
+            ],
+          });
+        }
+        else
+        {
+          response = await openai.chat.completions.create({
+            model: 'gpt-4o',  
+            messages: [
+              {
+                role: 'user',
+                content: [
+                  { type: 'text', text: 'Extract text, images, and equations from the image. Also, read the page number.' },
+                  { type: 'image_url', image_url: { url: base64Image } },
+                ],
+              },
+            ],
+          });
+        }
   
-        let extractedText = response.choices[0].message;
+        let extractedText = response?.choices[0].message;
   
           return extractedText;
       } else {
