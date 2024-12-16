e1b6f47bd22aba108080ea78a4b8e9cc6d9b3f2d
SheetalRhibhus <sheetal.venkatesha@rhibhus.com>
2024-12-16 11:47:39 +0530

    bugfix

--- helper/helper.js
+++ helper/helper.js
@@ -891,7 +891,7 @@ exports.getRandomQuestionsFromGroups = (group_response, noOfQuestions, randomDup
                          
                       }else{
 
-                        resolve({questions_list, randomDupCheck, quiz_duration}); 
+                        resolve({questions_list, randomDupCheck, quiz_duration,group_list}); 
     
                       }
                   }; 
