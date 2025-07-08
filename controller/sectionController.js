07679d7fb05667fc37fc6cad1198c4680ef09993
unknown <ahmad.ajaz@rhibhus.com>
2025-07-08 18:06:38 +0530

    Timetable Configuration Feature

--- controller/sectionController.js
+++ controller/sectionController.js
@@ -11,3 +11,15 @@ exports.updateActionAndRecommendations = async (req, res, next) => {
         next(error);
     }
 };
+
+exports.saveTimetableConfiguration = (req, res, next) => {
+         let request = req.body;
+         sectionServices.saveTimetableConfiguration(request, function (err, response) {
+             if (err) {
+                 res.status(err).json(response);
+             } else {
+                res.json(response);
+            }
+    });
+};
+
